ab359155ba5cb875d94de92f346c69d79fd37948
Arman Mielke <arman.mielke@yahoo.de>
2019-12-17 16:00:42 -0500

    Print baseline metrics

--- evaluation.py
+++ evaluation.py
@@ -3,9 +3,7 @@ from sklearn import metrics
 from typing import List
 
 # left is 0, right is 1
-baseline_predicted_labels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1]
-baseline_ground_truth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-subreddits = ['accidentallycommunist', 'againsthatesubreddits', 'anarchism', 'anarchocommunism', 'anarchy101', 'ani_communism', 'antifascistsofreddit', 'antifastonetoss', 'antiwork', 'askaliberal', 'bannedfromthe_donald', 'beto2020', 'bluemidterm2018', 'breadtube', 'centerleftpolitics', 'chapotraphouse', 'chapotraphouse2', 'chomsky', 'circlebroke', 'askaconservative', 'benshapiro', 'conservative', 'conservativelounge', 'conservatives', 'conservatives_only', 'cringeanarchy', 'jordanpeterson', 'louderwithcrowder', 'metacanada', 'newpatriotism', 'paleoconservative', 'republican', 'rightwinglgbt', 'shitpoliticssays', 'the_donald', 'thenewright', 'tuesday', 'walkaway']
+
 
 def print_metrics(predicted_labels: List[int], ground_truth_labels: List[int]):
     print(f"Predicted:    {predicted_labels}")
@@ -18,3 +16,25 @@ def print_metrics(predicted_labels: List[int], ground_truth_labels: List[int]):
     print(f"Adjusted Rand Index (ARI):         {ari}")
     print(f"Adjusted Mutual Information (AMI): {ami}")
     print(f"Fowlkes-Mallows index (FMI):       {fmi}")
+
+
+def print_baseline_metrics():
+    baseline_predictions = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1
+    ]
+    ground_truth = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    ]
+
+    # subreddits = [
+    #     'accidentallycommunist', 'againsthatesubreddits', 'anarchism', 'anarchocommunism', 'anarchy101',
+    #     'ani_communism', 'antifascistsofreddit', 'antifastonetoss', 'antiwork', 'askaliberal',
+    #     'bannedfromthe_donald', 'beto2020', 'bluemidterm2018', 'breadtube', 'centerleftpolitics',
+    #     'chapotraphouse', 'chapotraphouse2', 'chomsky', 'circlebroke', 'askaconservative', 'benshapiro',
+    #     'conservative', 'conservativelounge', 'conservatives', 'conservatives_only', 'cringeanarchy',
+    #     'jordanpeterson', 'louderwithcrowder', 'metacanada', 'newpatriotism', 'paleoconservative',
+    #     'republican', 'rightwinglgbt', 'shitpoliticssays', 'the_donald', 'thenewright', 'tuesday', 'walkaway'
+    # ]
+
+    print("Baseline (on training set):")
+    print_metrics(baseline_predictions, ground_truth)
