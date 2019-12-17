08563eb521b5572627331ef13055f6fa129efee2
DESKTOP-DBAP0RR\JustinYang <1301f17@gmail.com>
2019-12-17 15:46:31 -0500

    add baseline to evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -2,6 +2,10 @@ from sklearn import metrics
 
 from typing import List
 
+# left is 0, right is 1
+baseline_predicted_labels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1]
+baseline_ground_truth = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+subreddits = ['accidentallycommunist', 'againsthatesubreddits', 'anarchism', 'anarchocommunism', 'anarchy101', 'ani_communism', 'antifascistsofreddit', 'antifastonetoss', 'antiwork', 'askaliberal', 'bannedfromthe_donald', 'beto2020', 'bluemidterm2018', 'breadtube', 'centerleftpolitics', 'chapotraphouse', 'chapotraphouse2', 'chomsky', 'circlebroke', 'askaconservative', 'benshapiro', 'conservative', 'conservativelounge', 'conservatives', 'conservatives_only', 'cringeanarchy', 'jordanpeterson', 'louderwithcrowder', 'metacanada', 'newpatriotism', 'paleoconservative', 'republican', 'rightwinglgbt', 'shitpoliticssays', 'the_donald', 'thenewright', 'tuesday', 'walkaway']
 
 def print_metrics(predicted_labels: List[int], ground_truth_labels: List[int]):
     print(f"Predicted:    {predicted_labels}")
